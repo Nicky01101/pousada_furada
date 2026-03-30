075107cf3913e11fdf9812a810d27e906cd08b0a
Nicolas Vieria <nicovieira2007@gmail.com>
2026-03-30 19:03:33 +0000

    Adicionando o teste de roteamento App.test.jsx e configurando o Jest

--- src/tests/index.test.jsx
+++ src/tests/index.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import Home from "../../pages/index";
+import Home from "../pages/index";
 describe("Página Inicial", () => {
  it("renderiza o título principal", () => {
  render(<Home />);
